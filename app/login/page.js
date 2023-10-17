8e930223f1a67370b6f051bb33f3e48c499e3061
Sadra <sadra.kabiri@gmail.com>
2023-10-17 16:14:15 +0330

    modify login component with tailwind login form

--- app/login/page.js
+++ app/login/page.js
@@ -1,5 +1,9 @@
 import React from "react";
-
+import Login from "../components/auth/Login";
 export default function page() {
-  return <div></div>;
+  return (
+    <>
+      <Login />
+    </>
+  );
 }
--- app/signup/page.js
+++ app/signup/page.js
@@ -1,5 +1,9 @@
 import React from "react";
-
+import Signup from "../components/auth/Signup";
 export default function page() {
-  return <div></div>;
+  return (
+    <>
+      <Signup />
+    </>
+  );
 }
