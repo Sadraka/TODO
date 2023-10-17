d3bd4cc358ea3f9728bb817cbdb5447398974bce
Sadra <sadra.kabiri@gmail.com>
2023-10-17 15:39:03 +0330

    create User.js and User model

---
+++ models/User.js
@@ -0,0 +1,33 @@
+import { Schema, model, models } from "mongoose";
+const userSchema = new Schema({
+  email: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  name: {
+    type: String,
+  },
+  lastname: {
+    type: String,
+  },
+  todo: {
+    type: Array,
+    default: [],
+  },
+  createAt: {
+    type: Date,
+    immutable: true,
+    default: Date.now,
+  },
+  updateAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+const User = models.Users || new model("Users", userSchema);
+// new model("X" ,Xschema) ===> models.X
+export default User;
