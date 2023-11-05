fc3d63f3b16c05bbaedc08af41e80aa58d0e81e3
Sadra <sadra.kabiri@gmail.com>
2023-11-05 20:52:09 +0330

    fix google login button and disbale it when click it

--- components/auth/WithGoogle.js
+++ components/auth/WithGoogle.js
@@ -1,20 +1,30 @@
 import { signIn } from "next-auth/react";
 import Image from "next/image";
-import React from "react";
+import React, { useState } from "react";
 import googlesvg from "../../global/google.svg";
+import styles from "./WithGoogle.module.css";
 export default function WithGoogle() {
+  const [isclick, setIsclick] = useState(false);
+
   const signInHandler = () => {
+    setIsclick(true);
+    console.log("click");
     signIn("google");
   };
   return (
-    <div>
-      <button onClick={() => signInHandler()}>
+    <div className={styles.container}>
+      <button
+        onClick={() => signInHandler()}
+        className={isclick ? styles.clicked : styles.button}
+        disabled={isclick}
+      >
         <Image
           src={googlesvg}
-          width={50}
+          width={40}
           className="mt-1"
           alt="Login with google"
         />
+        Sign in With Google
       </button>
     </div>
   );
